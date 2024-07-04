f7466c8587f280e7a0cca583d3856a0dc24f8dbc
Harsh Pareek <114802400+Hrsh7300@users.noreply.github.com>
2024-07-04 15:24:01 +0530

    Delete URL-Shortner-main directory

--- URL-Shortner-main/server/routes/url.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const {generateCode,redirectUrl,analyticsUrl} = require('../controllers/url');
-
-//routes
-router.post("/",generateCode);
-router.get("/:shortid",redirectUrl);
-router.get('/analytics/:shortid',analyticsUrl)
-module.exports = router;
